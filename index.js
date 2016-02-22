6da0f2adbdc655f75c3d36a228aabe7928cc2a66
Laurent Jouanneau <laurent@innophi.com>
2016-02-22 16:59:16 +0100

    Add toolbar buttons

--- index.js
+++ index.js
@@ -1,9 +1,63 @@
 var self = require("sdk/self");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
 
-exports.dummy = dummy;
+exports.main = function (options, callbacks) {
+
+  var buttons = require('sdk/ui/button/action');
+
+
+  var privacyButton = buttons.ActionButton({
+    id: "qwant-privacy",
+    label: "Qwant privacy",
+    icon: {
+      "16": "./img/icon_privacy_16.png",
+      "32": "./img/icon_privacy_32.png",
+      "48": "./img/icon_privacy_48.png"
+    },
+    onClick: handlePrivacyClick
+  });
+
+  function handlePrivacyClick(state) {
+      console.log("privacy click");
+  }
+
+
+  var noteButton = buttons.ActionButton({
+    id: "qwant-note",
+    label: "Qwant Note",
+    icon: {
+      "16": "./img/icon_note_16.png",
+      "32": "./img/icon_note_32.png",
+      "48": "./img/icon_note_48.png"
+    },
+    onClick: handleNoteClick
+  });
+
+  function handleNoteClick(state) {
+      console.log("note click");
+  }
+
+
+  var bookmarkButton = buttons.ActionButton({
+    id: "qwant-bookmark",
+    label: "Qwant bookmark",
+    icon: {
+      "16": "./img/icon_bookmark_16.png",
+      "32": "./img/icon_bookmark_32.png",
+      "48": "./img/icon_bookmark_48.png"
+    },
+    onClick: handleBookmarkClick
+  });
+
+  function handleBookmarkClick(state) {
+      console.log("bookmark click");
+  }
+
+};
+
+
+
+exports.onUnload = function (reason) {
+  
+};
+
