16e766491a8c015a2238ec319eb330e83f5d34bf
Laurent Jouanneau <laurent@innophi.com>
2016-02-22 17:52:08 +0100

    Implements tracking protection button
    Closes #1
    Closes #2

--- index.js
+++ index.js
@@ -1,63 +1,53 @@
 var self = require("sdk/self");
+var buttons = require('sdk/ui/button/action');
 
 
 exports.main = function (options, callbacks) {
 
-  var buttons = require('sdk/ui/button/action');
-
-
-  var privacyButton = buttons.ActionButton({
-    id: "qwant-privacy",
-    label: "Qwant privacy",
-    icon: {
-      "16": "./img/icon_privacy_16.png",
-      "32": "./img/icon_privacy_32.png",
-      "48": "./img/icon_privacy_48.png"
-    },
-    onClick: handlePrivacyClick
-  });
-
-  function handlePrivacyClick(state) {
-      console.log("privacy click");
-  }
-
-
-  var noteButton = buttons.ActionButton({
-    id: "qwant-note",
-    label: "Qwant Note",
-    icon: {
-      "16": "./img/icon_note_16.png",
-      "32": "./img/icon_note_32.png",
-      "48": "./img/icon_note_48.png"
-    },
-    onClick: handleNoteClick
-  });
-
-  function handleNoteClick(state) {
-      console.log("note click");
-  }
-
-
-  var bookmarkButton = buttons.ActionButton({
-    id: "qwant-bookmark",
-    label: "Qwant bookmark",
-    icon: {
-      "16": "./img/icon_bookmark_16.png",
-      "32": "./img/icon_bookmark_32.png",
-      "48": "./img/icon_bookmark_48.png"
-    },
-    onClick: handleBookmarkClick
-  });
-
-  function handleBookmarkClick(state) {
-      console.log("bookmark click");
-  }
-
+    prepareButtons()
+    require('./lib/privacy').init(options.loadReason == 'install' ||
+                                  options.loadReason == 'enable');
 };
 
 
 
 exports.onUnload = function (reason) {
-  
+    if (reason == 'uninstall' || reason == 'disable') {
+        require('./lib/privacy').reset();
+    }
 };
 
+
+
+function prepareButtons() {
+
+    var noteButton = buttons.ActionButton({
+        id: "qwant-note",
+        label: "Qwant Note",
+        icon: {
+            "16": "./img/icon_note_16.png",
+            "32": "./img/icon_note_32.png",
+            "48": "./img/icon_note_48.png"
+        },
+        onClick: handleNoteClick
+    });
+    
+    function handleNoteClick(state) {
+        console.log("note click");
+    }
+
+    var bookmarkButton = buttons.ActionButton({
+        id: "qwant-bookmark",
+        label: "Qwant bookmark",
+        icon: {
+            "16": "./img/icon_bookmark_16.png",
+            "32": "./img/icon_bookmark_32.png",
+            "48": "./img/icon_bookmark_48.png"
+        },
+        onClick: handleBookmarkClick
+    });
+
+    function handleBookmarkClick(state) {
+        console.log("bookmark click");
+    }
+}
