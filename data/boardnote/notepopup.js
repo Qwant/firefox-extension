aa013f836a06aadfa8e0780ee6a8d7338654f0dc
Laurent Jouanneau <laurent@innophi.com>
2016-02-24 16:21:06 +0100

    Note popup: support of error, boards loading...

--- data/boardnote/notepopup.js
+++ data/boardnote/notepopup.js
@@ -1,6 +1,25 @@
 "use strict";
 
-/*
-self.on("show", function() {
-    
-});*/
+self.port.on("show", function() {
+    document.body.classList.remove('error');
+    document.body.classList.add('load');
+})
+
+self.port.on("initform", function(data) {
+    document.body.classList.remove('error', 'load');
+    let list = document.getElementById('board-field');
+    data.boards.forEach(function(board) {
+        let opt = document.createElement('option');
+        opt.textContent = board.board_name + (board.board_status  == "0"?' 🔒 ':'');
+        list.appendChild(opt);
+    });
+
+    document.getElementById('url-field').value = data.url;
+    document.getElementById('title-field').value = data.title;
+});
+
+self.port.on("error", function(err) {
+    document.body.classList.remove('load');
+    document.body.classList.add('error');
+    document.getElementById('error').textContent = err;
+});
