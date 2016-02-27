685775a8f9e91cd4d95e96d863d0a595ff8ffb2e
Laurent Jouanneau <laurent@innophi.com>
2016-02-27 21:20:05 +0100

    Authentication: support of events sent to and receive from qwant.com page

--- data/content-message.js
+++ data/content-message.js
@@ -3,9 +3,16 @@
  * we expect to receive this message from qwant.com when the user login
  */
 window.addEventListener("addon-login", function(event) {
-    self.port.emit('login', event.detail);
-
-
+    if (event.detail &&
+        typeof event.detail.username != undefined &&
+        typeof event.detail.session_token != undefined)
+        {
+            self.port.emit('login', {
+                username: event.detail.username,
+                token: event.detail.session_token
+                }
+            );
+    }
 }, false);
 
 /**
@@ -15,3 +22,15 @@ window.addEventListener("addon-logout", function(event) {
     self.port.emit('logout');
 }, false);
 
+
+self.port.on("login", function(message) {
+    var event = new window.CustomEvent("from-addon-login", {
+      detail: JSON.stringify(message)
+    });
+    window.dispatchEvent(event);
+});
+
+self.port.on("logout", function(message) {
+    var event = new window.CustomEvent("from-addon-logout", {});
+    window.dispatchEvent(event);
+});
