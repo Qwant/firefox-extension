8025053c5bcba9ec765863674905a101c2c8aee8
Sampson SAVINEL <s.savinel@qwant.com>
2016-06-23 10:27:50 +0200

    added checks to fit mozilla review && added support for browser F4Q param

--- index.js
+++ index.js
@@ -3,29 +3,36 @@
 var self = require("sdk/self")
 	, _ = require("sdk/l10n").get
 	, tabs = require("sdk/tabs")
-	, searchPlugin = require('./lib/searchplugin');
+	, searchPlugin = require('./lib/searchplugin')
+	, Preferences = require("sdk/preferences/service");
+
+const FIREFOX4QWANT = "extensions.qwant.distribution";
 
 exports.main = function (options) {
 	// loadReason = install enable startup upgrade downgrade
 	let firstLoad = options.loadReason == 'install' ||
-		options.loadReason == 'upgrade';
+		options.loadReason == 'upgrade' ||
+		options.loadReason == 'enabled' ;
+
+	let f4q = Preferences.get(FIREFOX4QWANT) || false;
 
 	require('./lib/privacy').main(firstLoad);
 	require('./lib/panel').main(firstLoad);
 
-	if (options.loadReason === 'enable' || firstLoad) {
+	if (firstLoad && f4q === false) {
 		searchPlugin.addQwant(searchPlugin.setAsDefault);
-	}
-
-	if (firstLoad) {
 		tabs.open("https://www.qwant.com/extension/firefox/first-run");
 	}
 };
 
 exports.onUnload = function (reason) {
+	let f4q = Preferences.get(FIREFOX4QWANT);
+
 	//reason = uninstall disable shutdown upgrade downgrade
 	if (reason == 'uninstall' || reason == 'disable') {
+		if (f4q === false) {
+			require('./lib/searchplugin').removeQwant();
+		}
 		require('./lib/privacy').reset();
-		require('./lib/searchplugin').removeQwant();
 	}
 };
