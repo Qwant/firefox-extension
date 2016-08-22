32c294a7d1a102f3ca7b3304eb60b3cffb0014d8
Sampson SAVINEL <s.savinel@qwant.com>
2016-08-22 11:04:56 +0200

    Fixed config handling on content script. EXT-87 reusing ID for Qwant bookmark group

--- data/js/events.js
+++ data/js/events.js
@@ -5,49 +5,49 @@ var extensionInstalled = true;
 unsafeWindow.extensionInstalled = cloneInto(extensionInstalled, unsafeWindow);
 
 document.addEventListener("qwant_website_login", function () {
-    if (config.DEBUG) {
+    if (self.options.DEBUG) {
         console.log("Received event from Qwant subdomain : qwant_website_login");
     }
     self.port.emit("qwant_website_login");
 });
 
 document.addEventListener("qwant_website_logout", function () {
-    if (config.DEBUG) {
+    if (self.options.DEBUG) {
         console.log("Received event from Qwant subdomain : qwant_website_logout");
     }
     self.port.emit("qwant_website_logout");
 });
 
 document.addEventListener("qwant_website_bookmark_created", function () {
-    if (config.DEBUG) {
+    if (self.options.DEBUG) {
         console.log("Received event from Qwant subdomain : qwant_website_bookmark_created");
     }
     self.port.emit("qwant_website_bookmark_created");
 });
 
 document.addEventListener("qwant_website_bookmark_deleted", function () {
-    if (config.DEBUG) {
+    if (self.options.DEBUG) {
         console.log("Received event from Qwant subdomain : qwant_website_bookmark_deleted");
     }
     self.port.emit("qwant_website_bookmark_deleted");
 });
 
 document.addEventListener("qwant_website_open_extension", function () {
-    if (config.DEBUG) {
+    if (self.options.DEBUG) {
         console.log("Received event from Qwant subdomain : qwant_website_open_extension");
     }
     self.port.emit("qwant_website_open_extension");
 });
 
 self.port.on("qwant_extension_login", function () {
-    if (config.DEBUG) {
+    if (self.options.DEBUG) {
         console.log("Sending event to Qwant subdomain : qwant_extension_login");
     }
     document.dispatchEvent(new CustomEvent("qwant_extension_login"));
 });
 
 self.port.on("qwant_extension_logout", function () {
-    if (config.DEBUG) {
+    if (self.options.DEBUG) {
         console.log("Sending event to Qwant subdomain : qwant_extension_logout");
     }
     document.dispatchEvent(new CustomEvent("qwant_extension_logout"));
