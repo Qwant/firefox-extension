14f4def832f86da07de4d1c7d663fca08a85307e
Sampson SAVINEL <s.savinel@qwant.com>
2016-08-22 10:25:55 +0200

    Added config.DEBUG condition to display log

--- data/js/events.js
+++ data/js/events.js
@@ -5,36 +5,50 @@ var extensionInstalled = true;
 unsafeWindow.extensionInstalled = cloneInto(extensionInstalled, unsafeWindow);
 
 document.addEventListener("qwant_website_login", function () {
-    console.log("Received event from Qwant subdomain : qwant_website_login");
+    if (config.DEBUG) {
+        console.log("Received event from Qwant subdomain : qwant_website_login");
+    }
     self.port.emit("qwant_website_login");
 });
 
 document.addEventListener("qwant_website_logout", function () {
-    console.log("Received event from Qwant subdomain : qwant_website_logout");
+    if (config.DEBUG) {
+        console.log("Received event from Qwant subdomain : qwant_website_logout");
+    }
     self.port.emit("qwant_website_logout");
 });
 
 document.addEventListener("qwant_website_bookmark_created", function () {
-    console.log("Received event from Qwant subdomain : qwant_website_bookmark_created");
+    if (config.DEBUG) {
+        console.log("Received event from Qwant subdomain : qwant_website_bookmark_created");
+    }
     self.port.emit("qwant_website_bookmark_created");
 });
 
 document.addEventListener("qwant_website_bookmark_deleted", function () {
-    console.log("Received event from Qwant subdomain : qwant_website_bookmark_deleted");
+    if (config.DEBUG) {
+        console.log("Received event from Qwant subdomain : qwant_website_bookmark_deleted");
+    }
     self.port.emit("qwant_website_bookmark_deleted");
 });
 
 document.addEventListener("qwant_website_open_extension", function () {
-    console.log("Received event from Qwant subdomain : qwant_website_open_extension");
+    if (config.DEBUG) {
+        console.log("Received event from Qwant subdomain : qwant_website_open_extension");
+    }
     self.port.emit("qwant_website_open_extension");
 });
 
 self.port.on("qwant_extension_login", function () {
-    console.log("Sending event to Qwant subdomain : qwant_extension_login");
+    if (config.DEBUG) {
+        console.log("Sending event to Qwant subdomain : qwant_extension_login");
+    }
     document.dispatchEvent(new CustomEvent("qwant_extension_login"));
 });
 
 self.port.on("qwant_extension_logout", function () {
-    console.log("Sending event to Qwant subdomain : qwant_extension_logout");
+    if (config.DEBUG) {
+        console.log("Sending event to Qwant subdomain : qwant_extension_logout");
+    }
     document.dispatchEvent(new CustomEvent("qwant_extension_logout"));
 });
