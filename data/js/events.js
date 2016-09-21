b79744abca7999e75cce0b002e6f2ff7e202d244
Solinca <corbi.nicolas@gmail.com>
2016-09-21 16:21:09 +0200

    Feature/qwant 272 (#21) (#22)
    * Revert "EXT-82 improved Qwant folder gestion in bookmarks (#15)" (#17)
    
    * Develop (#20)
    
    * EXT-82 improved Qwant folder gestion in bookmarks (#15)
    
    * EXT-88 Better checking of the hash of the user LocalStorage value (#18)
    
    * EXT-89 Added missing translation and improved locales support (#19)
    
    * QWANT-272 Added events handling for qwant.com tracking protection switcher
    
    * QWANT-272 removed debug mode

--- data/js/events.js
+++ data/js/events.js
@@ -5,50 +5,50 @@ var extensionInstalled = true;
 unsafeWindow.extensionInstalled = cloneInto(extensionInstalled, unsafeWindow);
 
 document.addEventListener("qwant_website_login", function () {
-    if (self.options.DEBUG) {
-        console.log("Received event from Qwant subdomain : qwant_website_login");
-    }
     self.port.emit("qwant_website_login");
 });
 
 document.addEventListener("qwant_website_logout", function () {
-    if (self.options.DEBUG) {
-        console.log("Received event from Qwant subdomain : qwant_website_logout");
-    }
     self.port.emit("qwant_website_logout");
 });
 
 document.addEventListener("qwant_website_bookmark_created", function () {
-    if (self.options.DEBUG) {
-        console.log("Received event from Qwant subdomain : qwant_website_bookmark_created");
-    }
     self.port.emit("qwant_website_bookmark_created");
 });
 
 document.addEventListener("qwant_website_bookmark_deleted", function () {
-    if (self.options.DEBUG) {
-        console.log("Received event from Qwant subdomain : qwant_website_bookmark_deleted");
-    }
     self.port.emit("qwant_website_bookmark_deleted");
 });
 
 document.addEventListener("qwant_website_open_extension", function () {
-    if (self.options.DEBUG) {
-        console.log("Received event from Qwant subdomain : qwant_website_open_extension");
-    }
     self.port.emit("qwant_website_open_extension");
 });
 
+document.addEventListener("qwant_website_is_tp_enabled", function () {
+    self.port.emit("qwant_website_is_tp_enabled");
+});
+
+document.addEventListener("qwant_website_tp_on", function () {
+    self.port.emit("qwant_website_tp_on");
+});
+
+document.addEventListener("qwant_website_tp_off", function () {
+    self.port.emit("qwant_website_tp_off");
+});
+
 self.port.on("qwant_extension_login", function () {
-    if (self.options.DEBUG) {
-        console.log("Sending event to Qwant subdomain : qwant_extension_login");
-    }
     document.dispatchEvent(new CustomEvent("qwant_extension_login"));
 });
 
 self.port.on("qwant_extension_logout", function () {
-    if (self.options.DEBUG) {
-        console.log("Sending event to Qwant subdomain : qwant_extension_logout");
-    }
     document.dispatchEvent(new CustomEvent("qwant_extension_logout"));
+});
+
+self.port.on("qwant_extension_tp_status", function (data) {
+    if (data === true) {
+        document.dispatchEvent(new CustomEvent("qwant_extension_tp_enabled"));
+    } else {
+        document.dispatchEvent(new CustomEvent("qwant_extension_tp_disabled"));
+    }
+
 });
