0f9fb2ac53b196424354dd155556c72a0ec708f1
Sampson SAVINEL <s.savinel@qwant.com>
2016-12-22 16:57:13 +0100

    QWANT-560 done

--- bootstrap.js
+++ bootstrap.js
@@ -12,10 +12,16 @@ const COMMONJS_URI                          = "resource://gre/modules/commonjs";
 const {require}                             = Cu.import(COMMONJS_URI + "/toolkit/require.js", {});
 const {Bootstrap}                           = require(COMMONJS_URI + "/sdk/addon/bootstrap.js");
 
+
+
 var {startup, shutdown, uninstall}          = new Bootstrap(rootURI);
 
 function install (aData, aReason) {
     if (aReason == 5) {
         Services.prefs.setBoolPref("extensions.qwant.firstrun", true);
+        Services.prefs.setCharPref("browser.startup.homepage", "https://www.qwant.com/?client=qwant-firefox");
+    }
+    if (aReason == 7 && aData.version === "3.1.0") {
+        Services.prefs.setCharPref("browser.startup.homepage", "https://www.qwant.com/?client=qwant-firefox");
     }
 }
